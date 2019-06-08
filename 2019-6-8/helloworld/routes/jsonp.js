efaf5f606edb9067b4e04f6f29c6dfa91bf547be
nizp <nzp890524@163.com>
2019-06-08 18:00:22 +0800

    2019-6-8

---
+++ 2019-6-8/helloworld/routes/jsonp.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+router.get('/', function(req, res, next) {
+  var _callback = req.query.callback;//callback=fn3  -> fn3
+  var _data = [1,2,3,4,5];
+  console.log(_callback)
+  if (_callback){
+      res.type('text/javascript');
+    //   fn + '(' data + ')' ->  fn(data)
+    res.send(_callback + '(' + JSON.stringify(_data) + ')');
+      // setTimeout(function(){
+        
+      // },5000)
+  }
+  else{
+      res.json(_data);
+  }
+});
+
+module.exports = router;
---
+++ 2019-6-8/helloworld/routes/post.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+
+let person = [
+  '龙海丽',
+  '李亚茹',
+  'tony',
+  'dida'
+];
+
+router.post('/', function(req, res, next) {
+  let obj = {
+    code:0,
+    msg:'有介个银了啦!'
+  }
+  let json = req.body;
+  console.log(req);
+  if(!person.includes(json.user)){
+      obj.code = 1;
+      obj.msg = '木有介个银!';
+  }
+  res.send(JSON.stringify(obj));
+});
+
+module.exports = router;
